945a3e3b13bbe09e83bd75e3f36021216ee65663
TJhon <fr.jhonk@gmail.com>
2020-06-20 13:31:15 -0500

    Tema

---
+++ gitcoma.sh
@@ -0,0 +1,40 @@
+# Usuales
+git add .
+git status
+
+git push 
+
+#generadores de texto
+
+touch quiere.rmd
+
+mkdir datos bib preamble code rmd  plots
+
+touch temas.rmd
+
+
+git commit -m "para centrarme en la investugacion"
+git push
+
+
+cd rmd
+
+touch "01resumen.rmd" "02intro.rmd" "03ant_teo.rmd" '04ant_emp.rmd' '05hechos.rmd' '06model.rmd' '07hipo.rmd' '08metod.rmd' '09resul.rmd'
+
+cd .. 
+
+git add rmd/
+
+git commit -m 'Partes de la investigacion'
+
+git status
+
+touch inv.rmd 
+
+git add inv.rmd
+
+git status
+
+## 2 
+
+git status
