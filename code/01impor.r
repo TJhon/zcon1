c45294ffb8c8dc0ef2f8d0ccdee2e9f8d3d21ed1
TJhon <fr.jhonk@gmail.com>
2020-06-20 20:58:21 -0500

    datos listos para la limpieza a excepcion de los mencionados en ref.md

--- code/01impor.r
+++ code/01impor.r
@@ -1,11 +1,20 @@
 library(here)
 library(tidyverse)
 library(readxl)
+
+# consumo de combustible
 dem <- read_xlsx(here("datos", "excel", "DemandaCoes (1).xlsx"))
-pib <- read_xlsx(here("datos", "excel", "Mensuales-20200620-142733.xlsx"))
-
-nrow(dem)
-
+str(dem) # 2003 - 2020
 rds <- here("datos", "rds")
-
 saveRDS(dem, here("datos", "rds", "dem1.rds"))  # datos diarios
+
+# crecimiento 
+
+## inei 
+
+
+
+ggplot(dem, aes(FECHA, EJECUTADO)) + geom_line()
+
+
+
